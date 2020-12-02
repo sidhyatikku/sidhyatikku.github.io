a9bb0cc0ea7a2408730249b87b66ad704038ab13
Sidhya Tikku <sidhyaktikku@gmail.com>
2020-12-03 00:47:13 +0530

    add webpages1

---
+++ js/menuani.js
@@ -0,0 +1,9 @@
+function menuani() {
+  var x = document.getElementById("linkh");
+  if (x.style.display === "inline-block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "inline-block";
+  }
+}
--- js/scroll_header.js
+++ js/scroll_header.js
@@ -2,8 +2,10 @@ $(function() {
     $(window).on("scroll", function() {
         if($(window).scrollTop() > 5) {
             $(".header").addClass("active");
+            $("i.fas.fa-bars").addClass("active");
         } else {
            $(".header").removeClass("active");
+           $("i.fas.fa-bars").removeClass("active");
         }
     });
 });
