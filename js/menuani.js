b391f7a0e35994a9b041a1009e60cab29c6176c0
Sidhya Tikku <sidhyaktikku@gmail.com>
2021-09-18 22:13:37 -0400

    about+responsive

--- js/menuani.js
+++ js/menuani.js
@@ -1,9 +1,9 @@
 function menuani() {
   var x = document.getElementById("linkh");
-  if (x.style.display === "inline-block") {
+  if (x.style.display === "block") {
     x.style.display = "none";
 
   } else {
-    x.style.display = "inline-block";
+    x.style.display = "block";
   }
 }
--- js/scroll_header.js
+++ js/scroll_header.js
@@ -3,9 +3,15 @@ $(function() {
         if($(window).scrollTop() > 5) {
             $(".header").addClass("active");
             $("i.fas.fa-bars").addClass("active");
+            $("a.hamburger").addClass("active");
+            $("a.connecth").addClass("active");
+            $("a.resumeh").addClass("active");
         } else {
            $(".header").removeClass("active");
            $("i.fas.fa-bars").removeClass("active");
+           $("a.hamburger").removeClass("active");
+           $("a.connecth").removeClass("active");
+           $("a.resumeh").removeClass("active");
         }
     });
 });
